21d3f3fff1145a18c99302b35c50bc311a5bfa55
Yash Jain <yashjain@wirelessprv-10-192-5-120.near.illinois.edu>
2024-11-13 16:03:02 -0600

    hello baby neel

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const PORT = 3007;
+app.use(express.json());
+
+app.get('/api/', (req:Request, res:Response) => {
+    res.send('API end.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
