56ab74d28c50fad0adcfb9d3d10c1ff50c38c6b7
Yash Jain <yashjain@wirelessprv-10-192-5-120.near.illinois.edu>
2024-11-16 20:38:32 -0600

    neel boy

---
+++ client/src/services/services.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+export interface Constructor {
+    constructorID: number;
+    name: string;
+    constructorNationality: string;
+  }
+
+export interface Driver {
+    driverID: number;
+    firstName: string;
+    lastName: string;
+    driverNationality: string;
+}
+
+export interface RaceResults {
+    resultID: number;
+    raceID: number;
+    driverID: number;
+    constructorID: number;
+    qualifyID: number;
+    finalPosition: number;
+    racePoints: number;
+    fastestLap: string; // Assuming TIME format as a string
+}
+
+export interface Race {
+    raceID: number;
+    year: number;
+    name: string;
+    circuitID: number;
+    totalLaps: number;
+}
+
+
+  
+
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3007';
+
+export const httpClient = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
